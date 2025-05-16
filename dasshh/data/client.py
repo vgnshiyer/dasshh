218eeda82ee0f7280fe60584b8afb3877aac26dd
Vignesh Iyer <vgnshiyer@gmail.com>
2025-05-15 21:02:47 -0700

    chore: added database client

---
+++ dasshh/data/client.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from typing import Generator
+
+from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.ext.declarative import DeclarativeBase
+
+
+class Base(DeclarativeBase):
+    """Base class for all database models."""
+
+    pass
+
+
+class DBClient:
+    """Dasshh database client."""
+
+    db_path = Path.home() / ".dasshh" / "db" / "dasshh.db"
+
+    def __init__(self):
+        self.db_path.parent.mkdir(parents=True, exist_ok=True)
+        self.engine: Engine = create_engine(f"sqlite:///{self.db_path}")
+        self.DatabaseSessionFactory: sessionmaker = sessionmaker(bind=self.engine)
+
+        Base.metadata.create_all(bind=self.engine)
+
+    def get_db(self) -> Generator[Session, None, None]:
+        db: Session = self.DatabaseSessionFactory()
+        try:
+            yield db
+        except Exception:
+            db.close()
+            raise
+        finally:
+            db.close()
