4626e69e05512192c9d69366c14a9024c79c2a26
Vignesh Iyer <vgnshiyer@gmail.com>
2025-05-15 21:11:53 -0700

    chore: add navbar component

---
+++ dasshh/ui/components/navbar.py
@@ -0,0 +1,99 @@
+from textual import on
+from textual.app import ComposeResult
+from textual.containers import Container, Horizontal
+from textual.widgets import Static
+
+from dasshh.ui.events import ChangeView
+
+
+class NavItem(Static):
+    """A navigation item."""
+
+    DEFAULT_CSS = """
+    NavItem {
+        content-align: center middle;
+        width: auto;
+        height: auto;
+        padding: 0 2;
+        color: $text-muted;
+        text-style: bold;
+    }
+
+    NavItem:hover, NavItem.active {
+        color: $text;
+    }
+    """
+
+    def __init__(self, route: str, label: str, icon: str = "", **kwargs):
+        super().__init__(f"{icon} {label}", **kwargs)
+        self.route = route
+
+    def on_click(self) -> None:
+        self.add_class("active")
+        self.post_message(ChangeView(self.route))
+
+
+class Logo(Static):
+    """The logo."""
+
+    DEFAULT_CSS = """
+    Logo {
+        color: $surface;
+        content-align: left middle;
+        text-style: italic bold;
+        width: auto;
+        height: 100%;
+        margin: 0 4 0 24;
+        margin-top: -1;
+    }
+
+    Logo:hover {
+        color: $surface-darken-1;
+    }
+    """
+
+    txt = """
+    ╭──────────╮
+    │ Dasshh 🗲 │
+    ╰──────────╯
+    """
+
+    def render(self) -> str:
+        return self.txt
+
+
+class Navbar(Container):
+    """The Navbar"""
+
+    DEFAULT_CSS = """
+    Navbar {
+        layout: horizontal;
+        background: $primary;
+        width: 100%;
+        height: 3;
+    }
+
+    Navbar > #nav-items {
+        width: 1fr;
+        align: left middle;
+        margin-right: 12;
+    }
+    """
+
+    def compose(self) -> ComposeResult:
+        """Create the navbar items."""
+        yield Logo()
+
+        with Horizontal(id="nav-items"):
+            yield NavItem(route="chat", label="Chat", icon="󰭹", id="chat")
+            yield NavItem(route="settings", label="Settings", icon="", id="settings")
+            yield NavItem(route="about", label="About", icon="", id="about")
+
+    def on_mount(self) -> None:
+        self.query_one("#chat").add_class("active")
+
+    @on(ChangeView)
+    def change_view(self, event: ChangeView):
+        for item in self.query("NavItem"):
+            item.remove_class("active")
+        self.query_one(f"#{event.view}").add_class("active")
